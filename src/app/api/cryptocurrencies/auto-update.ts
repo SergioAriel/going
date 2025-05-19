591eee54d14e83cfe8d565a7895ab387274361da
Sergio Solis <solis.sergioariel@gmail.com>
2025-05-19 16:07:30 -0300

    vercel cron update database cryptocurrencies

---
+++ src/app/api/cryptocurrencies/auto-update.ts
@@ -0,0 +1,32 @@
+import client from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+    try {
+        const response = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`,
+            {
+                headers: {
+                    'X-CMC_PRO_API_KEY': process.env.API_KEY_COINMARKETCAP || "",
+                },
+            }
+        );
+        const { data } = await response.json();
+        const list = data.map(({ symbol, name, quote: { USD: { price } } }: { symbol: string; name: string; quote: { USD: { price: number } } }) => {
+            return {
+                symbol,
+                name,
+                price,
+            };
+        });
+
+        client.db("cryptocurrencies").collection("cryptocurrencies").insertOne({
+            list,
+            createdAt: new Date(),
+        });
+
+        return NextResponse.json(list, { status: 200 });
+    } catch (error) {
+        console.error("Error fetching currency:", error);
+        return NextResponse.json({ status: 500, error: "Failed to fetch currency" });
+    }
+}
