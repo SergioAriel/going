4042e77934f95fe7cf3b2bcf7e623fa44c1e52fe
Sergio Solis <solis.sergioariel@gmail.com>
2025-06-10 13:46:48 -0300

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,11 +19,26 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "GOING",
-  description: "Compra y vende productos utilizando Solana y otras formas de pago",
+  description: "Experience the future of commerce with GOING.",
   icons: {
     icon: ['/favicon.ico']
   },
+  openGraph: {
+    title: 'GOING',
+    description: 'Experience the future of commerce with GOING.',
+    url: 'https://going-taupe.vercel.app/',
+    siteName: 'GOING',
+    images: [
+      {
+        url: '/goingLogo.png', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      }
+    ],
 
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default async function RootLayout({
@@ -38,20 +53,6 @@ export default async function RootLayout({
 
   return (
     <html lang="es">
-      <head>
-        {/* Open Graph */}
-        <meta property="og:title" content="GOING — The Decentralized Marketplace" />
-        <meta property="og:description" content="Experience the future of commerce with GOING. Try the live demo now." />
-        <meta property="og:image" content="/goingLogo.png"/>
-        <meta property="og:url" content="https://going-taupe.vercel.app/" />
-        <meta property="og:type" content="website" />
-
-        {/* Twitter Card */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="GOING — The Decentralized Marketplace" />
-        <meta name="twitter:description" content="Experience the future of commerce with GOING. Try the live demo now." />
-        <meta name="twitter:image" content="/goingLogo.png" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
